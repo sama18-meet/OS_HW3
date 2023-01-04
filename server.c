04bbc1fff12b6d0d6b0afcc618ae191dd84d1661
Sama Mazzawi <sama.mazz93@gmail.com>
2023-01-04 12:41:23 +0200

    part 1 code not working

--- server.c
+++ server.c
@@ -39,6 +39,48 @@ void getargs(int *port, int* threads, int* queue_size, enum SchedAlg* schedalg, 
 
 }
 
+pthread_mutex_t queue_mutex;
+pthread_cond_t queue_not_empty;
+
+int* waiting_connections_queue;
+int* active_connections_list;
+int active_connections_counter;
+int waiting_connections_counter;
+
+
+int dequeueConnection() {
+    int connfd;
+
+    pthread_mutex_lock(&queue_mutex);
+    while (waiting_connections_counter == 0) {
+        pthread_cond_wait(&queue_not_empty, &queue_mutex);
+    }
+
+    connfd = waiting_connections_queue[0];
+    for (int i = 0; i < waiting_connections_counter - 1; i++) {
+        waiting_connections_queue[i] = waiting_connections_queue[i + 1];
+    }
+    waiting_connections_counter--;
+    active_connections_list[active_connections_counter++] = connfd;
+    pthread_mutex_unlock(&queue_mutex);
+    return connfd;
+}
+
+void enqueueConnection(int connfd, int max_queue_size) { // TODO: remove second arg and assert
+    pthread_mutex_lock(&queue_mutex);
+    waiting_connections_queue[waiting_connections_counter++] = connfd;
+    pthread_cond_signal(&queue_not_empty);
+    pthread_mutex_unlock(&queue_mutex);
+    assert(waiting_connections_counter+active_connections_counter <= max_queue_size);
+}
+
+void* startHandlerThread(void* args) {
+    while (1) {
+        int connfd = dequeueConnection();
+	requestHandle(connfd);
+	Close(connfd);
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -49,25 +91,60 @@ int main(int argc, char *argv[])
 
     getargs(&port, &threads, &queue_size, &schedalg, argc, argv);
 
-    // 
-    // HW3: Create some threads...
-    //
+    // init queue
+    waiting_connections_queue = malloc(sizeof(int)*queue_size);
+    if (waiting_connections_queue == NULL) {
+        exit(1);
+    }
+    active_connections_list = malloc(sizeof(int)*queue_size);
+    if (active_connections_list == NULL) {
+        exit(1);
+    }
+    active_connections_counter = 0;
+    waiting_connections_counter = 0;
 
-    listenfd = Open_listenfd(port);
-    while (1) {
-	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+    pthread_mutex_init(&queue_mutex, NULL);
+    pthread_cond_init(&queue_not_empty, NULL);
 
-	// 
-	// HW3: In general, don't handle the request in the main thread.
-	// Save the relevant info in a buffer and have one of the worker threads 
-	// do the work. 
-	// 
-	requestHandle(connfd);
+    // thread pool
+    pthread_t* threadpool = malloc(sizeof(pthread_t)*threads);
+    if (threadpool == NULL) {
+        exit(1);
+    }
+    for (int i=0; i<threads; ++i) {
+        if (pthread_create(&threadpool[i], NULL, &startHandlerThread, NULL) != 0) {
+            perror("Failed to create thread");
+            exit(1);
+        }
+    }
+    listenfd = Open_listenfd(port); // open and return a listening socket on port
+    if (schedalg == BLOCK) {
+        while (1) {
+            if (active_connections_counter < threads) { // TODO: Change this to block instead of busy wait
+	        clientlen = sizeof(clientaddr);
+	        connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+                enqueueConnection(connfd, queue_size);
+            }
 
-	Close(connfd);
+	    // 
+	    // HW3: In general, don't handle the request in the main thread.
+	    // Save the relevant info in a buffer and have one of the worker threads 
+	    // do the work. 
+	    // 
+        }
     }
 
+    for (int i=0; i<threads; ++i) {
+        if (pthread_join(threadpool[i], NULL) != 0) {
+            perror("Failed to join thread");
+            exit(1);
+        }
+    }
+
+    pthread_mutex_destroy(&queue_mutex);
+    pthread_cond_destroy(&queue_not_empty);
+
+    return 0;
 }
 
 
